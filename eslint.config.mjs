a4f56a603644658a05e000d913e34619cfa5b48b
aetosdios27 <aetosdios27@gmail.com>
2026-06-04 21:12:46 +0530

    fix: migrate eslint to typescript-eslint flat config, remove @eslint/eslintrc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({ baseDirectory: __dirname });
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default tsPlugin.config(
   { ignores: [".next/", "node_modules/", "coverage/"] },
-];
-
-export default eslintConfig;
+  ...tsPlugin.configs.recommended,
+);
